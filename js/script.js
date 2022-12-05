136177947c61536f0e19db51f76fef12ed895050
Yofre Ormaza <97921335+HORTTIZ@users.noreply.github.com>
2022-12-04 22:04:15 -0500

    Agregar archivos iniciales

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    btn.value = 'Enviando...';
+    
+    const serviceID = 'default_service';
+    
+    const templateID = 'template_arjpdjk';
+    
+    emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        btn.value = 'Send Email';
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            timer: 2000,
+            width: '100%',
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+        Toast.fire({
+            icon: 'success',
+            title: '¡Enviado! Regresaremos a la App ☺'
+        })
+        setTimeout(function(){
+            window.location.href = "https://horttiz.github.io/FOCUS/";
+        }, 3000);
+    }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+    });
+});
